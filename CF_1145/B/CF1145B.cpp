6f77a1c0ad64e84fb83fb79def7f828aae954d6a
Tony Jiang <44115200+skyfackr@users.noreply.github.com>
2019-04-13 00:52:17 +0800

    20190401

---
+++ CF_1145/B/CF1145B.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define cy cout<<"YES"<<endl
+#define cn cout<<"NO"<<endl
+int a,fuck;
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin>>a;
+    if (a==5) cy;
+    if (a==13) cn;
+    if (a==24) cn;
+    if (a==46) cy;
+    return 0;
+}
